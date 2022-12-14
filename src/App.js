c13e51aa4cb0135b2d4be5cd45d01932cd141848
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-12-14 12:32:38 +0100

    12

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React faster
+          Learn React faster 22
         </a>
       </header>
     </div>
